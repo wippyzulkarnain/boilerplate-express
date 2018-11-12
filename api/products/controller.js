a87e0138102e72d73e1579a1cbe793fde05a78ff
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-11-12 16:13:31 +0700

    First Commit

--- api/products/controller.js
+++ api/products/controller.js
@@ -1,7 +1,16 @@
-module.exports.getAll = (req, res) => {
-    res.send("List of Products");
-  };
+const models = require("../models");
 
-  module.exports.post = (req, res) => {
-    res.send("Save New Product");
-  }
+exports.getAll = (req, res) => {
+  models.products
+    .findAll()
+    .then(products => res.send(products))
+    .catch(err => res.send(err));
+};
+
+exports.post = (req, res) => {
+  console.log(req.body)
+  models.products
+    .create(req.body)
+    .then(product => res.send(product))
+    .catch(err => res.send(err));
+};
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const products = require("./api/products")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
 
 app.use("/products",products)
 
